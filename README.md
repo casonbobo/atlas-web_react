f78b02655d596389adb4f465bfb3287027bbaedf
casonbobo <118225257+casonbobo@users.noreply.github.com>
2024-04-30 14:08:53 -0500

    Starting webpack. Depos and repos

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # atlas-web_react
+Learning how to use React and React Components. 
+The first project from Atlas in this set is Web-pack and then the following ones should be just React. 
---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+$(function(){
+    $('p').append('Holberton Dashboard');
+    $('p').append('Dashboard data for the students');
+    $('p').append('Copyright - Holberton School');
+});
