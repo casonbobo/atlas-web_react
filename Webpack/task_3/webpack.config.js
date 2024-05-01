c1c0615743812a453d5540de5992b42d58e0549a
casonbobo <118225257+casonbobo@users.noreply.github.com>
2024-05-01 15:18:46 -0500

    should've fixed the webpack

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,29 +1,35 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './js/dashboard_main.js',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    }, 
-    module: {
-        rules: [
-            { 
-                test: /\.css$/, 
-                use: ["style-loader", "css-loader"] //"file-loader", "image-webpack-loader"], 
-            },
-            { 
-              test: /\.jpg/,
-              type: 'asset/resource',
-            },
-        ]
-    },
-    devServer: {
-        port: 8564,
-        open: true,
-        hot: true,
-        compress: true,
-        historyApiFallback: true
-    }
+  mode: 'development',
+  entry: {
+    header: path.resolve(__dirname, './modules/header/header.js'),
+    body: path.resolve(__dirname, './modules/body/body.js'),
+    footer: path.resolve(__dirname, './modules/footer/footer.js'),
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './public')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 8564
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.css$/, 
+        use: ['style-loader', 'css-loader']//, 'file-loader', 'image-webpack-loader']
+      },
+      {
+        test: /\.jpg/,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ],
 };
