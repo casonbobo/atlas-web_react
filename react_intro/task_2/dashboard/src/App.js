4b95444dbaeafd438f6dceb0d7fcad67aaa4f0fa
casonbobo <118225257+casonbobo@users.noreply.github.com>
2024-05-06 21:06:14 -0500

    Added the work for task2 and made it work

--- react_intro/task_2/dashboard/src/App.js
+++ react_intro/task_2/dashboard/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { getCurrentYear, getFooterCopy } from './utils.js';
-
 import logo from './logo.png';
 
 function App() {
@@ -10,8 +9,17 @@ function App() {
         <img src={logo} className="logo" alt="logo" />
         <h1>School dashboard</h1>
       </header>
-        <main className="App-body">
+        <main className="App-body align-items">
           <p>Login to access the full dashboard</p>
+          <div>
+            <form>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required></input>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required></input>
+                <button>OK</button>
+            </form>
+          </div>
         </main>
         <footer className="App-footer">
           <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
