0e0de8ddb73f1803839f23c6f61f9eae0f271260
casonbobo <118225257+casonbobo@users.noreply.github.com>
2024-05-07 14:55:21 -0500

    should be good

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
     },
     devServer: {
+        hot: true,
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
         port: 8080,
