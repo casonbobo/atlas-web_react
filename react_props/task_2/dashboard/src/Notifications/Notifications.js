efacc4ac6d3a751c058b5917d63cf4203b9234cc
casonbobo <118225257+casonbobo@users.noreply.github.com>
2024-05-08 14:08:12 -0500

    Copied over task 2. That's where I did all the work

---
+++ react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,33 @@
+import React from "react";
+import './Notifications.css';
+import NotificationItem from "./NotificationItem";
+import { getLatestNotification } from "../utils/utils";
+
+export function Notifications() {
+  const handleButtonClick = () => {
+    console.log("close button has been clicked");
+  };
+
+  return (
+    <div className="Notifications">
+      <div className="Notifications-content">
+        <p> 
+          Here is the list of notifications
+        </p>
+        <ul>
+          <NotificationItem data-priority="default" value="New course available" />
+          <NotificationItem data-priority="urgent" value="New resume available" />
+          <NotificationItem data-priority="urgent" html={{ __html: getLatestNotification() }} />
+        </ul>
+        <button
+                className="button"
+                onClick={() => console.log("Close button has been clicked")}
+                aria-label="Close">
+                x
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Notifications;
