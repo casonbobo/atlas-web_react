64d291bddd56ddbfb1932bf8317cc5f242267692
casonbobo <118225257+casonbobo@users.noreply.github.com>
2024-05-13 14:12:58 -0500

    It no worky :(

---
+++ React_inline_styling/README.md
@@ -0,0 +1,10 @@
+This is a readme for the React INline styling project. I will be learing React more and learning how to use inline styling, 
+
+<h1>#Learning Objectives#</h1>
+<h3>##At the end of this project, you are expected to be able to explain to anyone, without the help of Google: ##</h3>
+
+the differences between using a CSS file and inline styling
+how to use a CSS-in-JS tool like Aphrodite
+how to use conditions within JS to apply different styles
+how to use responsive design and make the application show a different UI according to the screen size
+how to create small animations within the app
---
+++ React_inline_styling/task_3/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './CourseList.css';
+
+const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
+  const HeaderCell = ({ children, colSpan = 1 }) => (
+    <th colSpan={colSpan}>{children}</th>
+  );
+
+  const DataCell = ({ children }) => <td>{children}</td>;
+
+  if (isHeader) {
+    if (!textSecondCell) {
+      return (
+        <tr>
+          <HeaderCell colSpan={2}>{textFirstCell}</HeaderCell>
+        </tr>
+      );
+    } else {
+      return (
+        <tr>
+          <HeaderCell>{textFirstCell}</HeaderCell>
+          <HeaderCell>{textSecondCell}</HeaderCell>
+        </tr>
+      );
+    }
+  } else {
+    return (
+      <tr>
+        <DataCell>{textFirstCell}</DataCell>
+        <DataCell>{textSecondCell}</DataCell>
+      </tr>
+    );
+  }
+};
+
+CourseListRow.defaultProps = {
+  isHeader: false,
+  textSecondCell: null,
+};
+
+CourseListRow.propTypes = {
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+};
+
+export default CourseListRow;
