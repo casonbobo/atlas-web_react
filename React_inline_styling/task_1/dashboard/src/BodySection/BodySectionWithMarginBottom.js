238602d3fb01c8a6de7776b1d687dd14ed4c6961
casonbobo <118225257+casonbobo@users.noreply.github.com>
2024-05-16 13:56:00 -0500

    Whoops I did the work in the wrong file

--- React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -2,16 +2,24 @@ import React, { Component } from 'react';
 import BodySection from './BodySection';
 import './BodySection.css';
 import PropTypes from 'prop-types';
+import { StyleSheet } from 'aphrodite';
 
 class BodySectionWithMarginBottom extends Component {
   render () {
     return (
-      <div className='bodySectionWithMargin'>
+      <div className={css(styles.bodySectionWithMargin)}>
         <BodySection {...this.props} />
       </div>
     );
   }
 }
+const styles = StyleSheet.create({
+  bodySectionWithMargin: {
+    marginBottom: '40px',
+    width: '90%',
+    alignItems: 'left',
+  }
+});
 
 BodySectionWithMarginBottom.propTypes = {
   title: PropTypes.string.isRequired,
