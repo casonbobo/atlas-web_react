445b20d0f13063758a06b7a6cc545464cfbe97f5
casonbobo <118225257+casonbobo@users.noreply.github.com>
2024-05-16 13:41:27 -0500

    Should be task 1. The colored row and css has been moved and then removed

---
+++ React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -0,0 +1,46 @@
+// import './Login.css';
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+function App() {
+  return (
+    <div className={css(styles.App)}>
+      <main className={css(styles.Login)}>
+        <p>Login to access the full dashboard</p>
+        <div>
+          <form>
+              <label for="email">Email:</label>
+              <input type="email" id="email" name="email" required></input>
+              <label for="password">Password:</label>
+              <input type="password" id="password" name="password" required></input>
+              <button>OK</button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+const styles = StyleSheet.create({
+  App: {
+    textAlign: 'center',
+  },
+  root: {
+    '--red': '#E1160B',
+  },
+  AppLogo: {
+    height: '12rem',
+    pointerEvents: 'none',
+  },
+  AppBody: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    minHeight: '100vh',
+    flexDirection: 'column',
+  },
+  Login: {
+    alignItems: 'left',
+  }
+});
+
+export default App;
---
+++ React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,113 @@
+import React from "react";
+import NotificationItem from "./NotificationItem";
+import closeIcon from '../../assets/close.png';
+import { getLatestNotification } from "../utils/utils";
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from "aphrodite";
+
+class Notifications extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    return nextProps.listNotifications.length > this.props.listNotifications.length;
+  }
+
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+
+  handleButtonClick = () => {
+    console.log("close button has been clicked");
+  };
+
+  render() {
+    const { displayDrawer, listNotifications } = this.props;
+    return (
+      <>
+        <div className={css(styles.menuItem)}>
+          <p>Your Notifications</p>
+        </div>
+        {displayDrawer && (
+          <div className={css(styles.Notifications)}>
+            <div className={css(styles.NotificationsContent)}>
+              {listNotifications.length > 0 && (
+                <p>Here is the list of notifications</p>
+              )}
+              <ul>
+                {listNotifications.length === 0 ? (
+                  <NotificationItem value='No new notification for now' />
+                ) : (
+                  listNotifications.map(notification => (
+                    <NotificationItem
+                      key={notification.id}
+                      type={notification.type}
+                      value={notification.value}
+                      html={notification.html}
+                    />
+                  ))
+                )}
+              </ul>
+            </div>
+            <button
+              aria-label="Close"
+              className={css(styles.button)}
+              onClick={this.handleButtonClick}>
+                <img src={closeIcon} alt="Close" className={css(styles.icon)} />
+            </button>
+          </div>
+        )}
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  Notifications: {
+    display: 'flex',
+    textAlign: 'left',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+  },
+  NotificationsP: {
+    fontFamily: `'Galano Grotesque Alt', sans-serif`,
+    fontWeight: 400,
+    padding: '1.5rem 0 .3rem 1.5rem',
+    margin: 0,
+  },
+  NotificationsContentUl: {
+    padding: '2rem',
+    border: '5px dotted red',
+  },
+  defaultNotification: {
+    '[data-notification-type="default"]': {
+      fontFamily: `'Galano Grotesque Alt', sans-serif`,
+      fontWeight: 400,
+      color: 'blue',
+    },
+  },
+  urgentNotification: {
+    '[data-notification-type="urgent"]': {
+      fontFamily: `'Galano Grotesque Alt', sans-serif`,
+      fontWeight: 400,
+      color: 'red',
+    },
+  },
+  thirdListItem: {
+    'ul > li:nth-child(3)': {
+      fontFamily: `'Galano Grotesque Alt', sans-serif`,
+      fontWeight: 400,
+      fontSize: '1.2rem',
+      color: 'red',
+    },
+  },
+});
+
+Notifications.propTypes = {
+  listNotifications: PropTypes.array,
+  displayDrawer: PropTypes.bool,
+};
+
+Notifications.defaultProps = {
+  listNotifications: [],
+  displayDrawer: false,
+};
+
+export default Notifications;
