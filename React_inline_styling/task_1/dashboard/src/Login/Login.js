0f2fd17d8a29057a2522799c4c66c2a5f0e0fa46
casonbobo <118225257+casonbobo@users.noreply.github.com>
2024-05-16 14:28:47 -0500

    made login a comp and updated its test

--- React_inline_styling/task_1/dashboard/src/Login/Login.js
+++ React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -2,25 +2,27 @@
 import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
 
-function App() {
-  return (
-    <div className={css(styles.App)}>
-      <main className={css(styles.Login)}>
-        <p>Login to access the full dashboard</p>
-        <div>
-          <form>
-              <label for="email">Email:</label>
-              <input type="email" id="email" name="email" required></input>
-              <label for="password">Password:</label>
-              <input type="password" id="password" name="password" required></input>
-              <button>OK</button>
-          </form>
-        </div>
-      </main>
-    </div>
-  );
+class Login extends Component {
+  render() {
+    return (
+      <div className={css(styles.App)}>
+        <main className={css(styles.Login)}>
+          <p>Login to access the full dashboard</p>
+          <div>
+            <form>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required></input>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required></input>
+                <button>OK</button>
+            </form>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
-
+  
 const styles = StyleSheet.create({
   App: {
     textAlign: 'center',
@@ -34,4 +36,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default App;
+export default Login;
--- React_inline_styling/task_1/dashboard/src/Login/Login.test.js
+++ React_inline_styling/task_1/dashboard/src/Login/Login.test.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, configure } from 'enzyme';
+import { StyleSheetTestUtils } from '@testing-library/jest-dom/extend-expect';
 import Login from './Login'; // Import your Login component
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+configure({ adapter: new Adapter() });
+
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
 
 describe('Login', () => {
   it('renders without crashing', () => {
